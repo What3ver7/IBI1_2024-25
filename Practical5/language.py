caaca2648ca6871d387c46e9504adcf72c168ff8
Zhaooo <2820821032@qq.com>
2025-05-27 17:58:37 +0800

    Upload my content of Practical5

--- Practical5/language.py
+++ Practical5/language.py
@@ -1,31 +1,20 @@
- #Import `matplotlib.pyplot` as `plt`.  
- #Create a dictionary of languages and their popularity percentages.  
- #Extract keys (languages) and convert percentage values to floats.  
- #Plot a bar chart with `plt.bar()`.  
- #Set y-axis ticks from 0 to 100 with intervals of 10.  
- #Add percentage labels above each bar.  
- #Label x-axis, y-axis, and set the title.  
- #Display the chart using `plt.show()`.  
- #ask for user's input
-import matplotlib.pyplot as plt
-dict={"TypeScript":"38.5%",
-      "SQL":"51%",
-      "Python":"51%",
-      "HTML":"52.9%",
-      "JavaScript":"62.3%",
-       } 
-x=list(dict.keys())
-y =[float(value.strip('%')) for value in dict.values()]
-plt.bar(x,y)
-plt.yticks(range(0,100,10))
-for i, value in enumerate(y):
-    plt.text(i, value, f"{value}%",ha="center", va="bottom")
-plt.xlabel("Application")
-plt.ylabel("Percentage")
-plt.title("Programming language popularity")
-plt.show()
-x=input("which language?")
-if x in dict:
-    print(f"the percentage of developers who use this language is {dict[x]}")
+import matplotlib.pyplot as plt #import library I need
+
+language=["JavaScript", "HTML", "Python","SQL", "TypeScript"] #build two list and then a dictionary
+popularity=[62.3,52.9, 51, 51, 38.5]
+
+catergory_dict=dict(zip(language,popularity))
+print(catergory_dict) #print dictionary
+
+plt.bar(language,popularity)
+plt.xlabel("language")
+plt.ylabel("popularity") #draw the plot with x,y label and title
+plt.title("the percentage of developers who use the top 5 programming languages globally")
+plt.legend() #show the legned in the plot
+plt.show() #show the plot
+
+x=input("which language?") #ask for user's input
+if x in catergory_dict:
+    print(f"the percentage of developers who use this language is {catergory_dict[x]}")
 else:
-    print("language not found.")
+    print("language not found") #a simple fix to avoid bugs
--- Practical5/size.py
+++ Practical5/size.py
@@ -1,30 +1,35 @@
- #Define population data for UK countries and Chinese provinces.
- #Sort the population data.
- #Define corresponding labels for each region.
- #Create a figure with two subplots (side by side).
- #Plot a pie chart for UK countries with labels and percentages.
- #Set the title for the UK pie chart.
- #Plot a pie chart for Chinese provinces with labels and percentages.
- #Set the title for the China pie chart.
- #Display the charts.import matplotlib.pyplot as plt
-
 import matplotlib.pyplot as plt
-uk_countries=[57.11,3.13,1.91,5.45]
-china_province=[65.77,41.88,45.28,61.27,85.15]
-uk=sorted(uk_countries)
-china=sorted(china_province)
-uk_country=["Northern Ireland","Wales","Scotland","England"]
-china_province=["Fujian","Jiangxi","Anhui","Zhejiang","Jiangsu"]
-plt.figure(1)
-plt.subplot(1,2,1)
-plt.pie(uk,labels=uk_country,autopct="%.2f%%",)
-plt.title("UK Countries Distribution")
-plt.subplots_adjust(top=1,bottom=0)
-plt.legend(loc=2,fontsize=5)
-plt.subplot(1,2,2)
-plt.pie(china,labels=china_province,autopct="%.2f%%")
-plt.title("China Province Distribution")
-plt.legend(loc=1,fontsize=5)
-print(f"uk_country distribution is {uk}")
-print(f"china_province distribution is {china}")
+
+#create lists storing the population and names
+uk=[57.11,3.13,1.91,5.45]
+cn=[65.77,41.88,45.28,61.27,85.15]
+uk_name=["England","Wales","Northern Ireland","Scotland"]
+cn_name=["Zhejiang","Fujian","Jiangxi","Anhui","Jiangsu"]
+
+#sort the list based on population, while not change the matches
+sorted_uk=sorted(zip(uk,uk_name),reverse=True)
+sorted_cn=sorted(zip(cn,cn_name),reverse=True)
+uk_sorted,uk_sorted_name=zip(*sorted_uk)
+cn_sorted,cn_sorted_name=zip(*sorted_cn)
+
+#print the sorted lists
+print(list(uk_sorted_name),list(uk_sorted))
+print(list(cn_sorted_name),list(cn_sorted))
+
+#colors used in pie chart
+color1=['blue','red','yellow','green']
+color2=color1.append('pink')
+
+#pie chart 1 to draw England
+plt.figure(figsize=(6,6))
+plt.pie(uk_sorted,labels=uk_sorted_name,colors=color1,autopct='%1.1f%%')
+plt.title('population distribution of England')
+plt.legend()
+plt.show()
+
+#pie chart 2 to draw zhejiang neighboring provinces
+plt.figure(figsize=(6,6))
+plt.pie(cn_sorted,labels=cn_sorted_name,colors=color2,autopct='%1.1f%%')
+plt.title('population distribution of Zhejiang neighboring provinces')
+plt.legend()
 plt.show()
